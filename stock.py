224ac491a42d2d2361e5c162894bd7ee97dd1411
ANJANA MOHAN <163128317+Anjana13Mohan@users.noreply.github.com>
2025-04-20 00:01:09 -0500

    ML application

--- stock.py
+++ stock.py
@@ -2,11 +2,13 @@ import streamlit as st
 import yfinance as yf
 import datetime
 st.title("Stock price Analyzer")
-
-
 ticker_data = yf.Ticker("GOOG")
-start_date = st.date_input("Please enter starting date",datetime.date(2023,12,1))
-end_date = st.date_input("Please enter ending date",datetime.date(2024,12,1))
+
+col1, col2 = st.columns(2)
+with col1:
+    start_date = st.date_input("Please enter starting date",datetime.date(2023,12,1))
+with col2:
+    end_date = st.date_input("Please enter ending date",datetime.date(2024,12,1))
 ticker_df = ticker_data.history(period='1d',start=start_date,end=end_date)
 st.subheader("Here is the raw daywise stock price")
 st.dataframe(ticker_df)
